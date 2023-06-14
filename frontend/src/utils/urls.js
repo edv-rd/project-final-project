622b42734997666be253eafbf00156b7cf5961cd
Ed Code <edvard.ekstrom@gmail.com>
2023-06-14 21:42:27 +0200

    correct port on backend :)

--- frontend/src/utils/urls.js
+++ frontend/src/utils/urls.js
@@ -1,4 +1,4 @@
 /* eslint-disable no-undef */
-const API_URL = process.env.API_URL || "https://project-final-project-backend.onrender.com/";
+const API_URL = process.env.API_URL || "https://project-final-project-backend.onrender.com:8080";
 
 export default API_URL;
