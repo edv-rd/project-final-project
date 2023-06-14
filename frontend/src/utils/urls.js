2cd7dc1f5d4fdb752b9742044a2c4c918b7c256b
Ed Code <edvard.ekstrom@gmail.com>
2023-06-14 21:50:27 +0200

    removed port..

--- frontend/src/utils/urls.js
+++ frontend/src/utils/urls.js
@@ -1,4 +1,4 @@
 /* eslint-disable no-undef */
-const API_URL = process.env.API_URL || "https://project-final-project-backend.onrender.com:8080";
+const API_URL = process.env.API_URL || "https://project-final-project-backend.onrender.com";
 
 export default API_URL;
