8de6313ff2417d0923b348bed5b2f2af71210810
Ed Code <edvard.ekstrom@gmail.com>
2023-06-15 17:32:02 +0200

    ..

--- frontend/src/utils/urls.js
+++ frontend/src/utils/urls.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 
-// const API_URL = process.env.API_URL || "https://project-final-project-backend.onrender.com";
-const API_URL = "http://localhost:3000";
+const API_URL = process.env.API_URL || "https://project-final-project-backend.onrender.com";
+//const API_URL = "http://localhost:3000";
 
 export default API_URL;
