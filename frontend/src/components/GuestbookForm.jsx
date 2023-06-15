94d049f67f3f812dc76714965a72e99b65093620
Ed Code <edvard.ekstrom@gmail.com>
2023-06-15 17:29:11 +0200

    trying reload function in gb

--- frontend/src/components/GuestbookForm.jsx
+++ frontend/src/components/GuestbookForm.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import API_URL from "../utils/urls.js";
 import { useState } from "react";
+import { redirect } from "react-router-dom";
 
 import Cookies from "universal-cookie";
 const cookies = new Cookies();
@@ -25,12 +26,13 @@ const GuestbookForm = ({ owner }) => {
         content: entryContent,
         postedTo: owner,
       }),
+    }).then(() => {
+      window.location.reload();
     });
   };
   return (
     <form onSubmit={handleFormSubmit}>
       <textarea id="content" value={entryContent} onChange={handleNewText} />
-
       <button type="submit">Post!</button>
     </form>
   );
